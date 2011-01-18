c51a5b2a4d54238515035a6e4094342f8eaa90f1
Fritz Grimpen <fritz@grimpen.net>
2011-01-18 23:08:22 +0100

    Implement support for rfc2047

--- lib/net/nntp/article.rb
+++ lib/net/nntp/article.rb
@@ -1,12 +1,46 @@
+require "base64"
 
 module Net
   class NNTP
     class Article
+      RFC2047_REGEXP = /(\=\?([^\x00-\x20]+)\?([^\x00-\x20]+)\?([^\? ]+)\?\=)/
+    
       attr_accessor :body, :headers
     
       def initialize
         @headers = {}
         @body = ""
+      end
+      
+      def self.decode_rfc2047(string)
+        result = {}
+        
+        string.scan(RFC2047_REGEXP).to_a.each do |occur|
+          origin, charset, encoding, source = occur
+          encoding = encoding.downcase
+          charset = charset.downcase
+          subresult = nil
+          
+          if encoding == "q"
+            subresult = source.gsub /\=([A-Za-z0-9]{2})/ do |match|
+              match.to_i(16).chr
+            end
+          elsif encoding == "g"
+            subresult = Base64.decode64(source)
+          end
+          
+          subresult = subresult.force_encoding(charset)
+          subresult = subresult.encode("utf-8", charset)
+          
+          result[origin] = subresult
+        end
+        
+        result_string = string
+        result.each do |s,r|
+          result_string = result_string.sub(s, r)
+        end
+        
+        result_string
       end
       
       def self.parse(string)
@@ -27,7 +61,11 @@ module Net
         
         header_lines.each do |line|
           name, value = line.split(":", 2)
-          article_obj[name.to_s.strip.capitalize] = value.to_s.strip
+          begin
+            article_obj[name.to_s.strip.capitalize] = decode_rfc2047(value.to_s.strip)
+          rescue Exception => e
+            puts e.inspect
+          end
         end
         
         article_obj.body ||= ""
